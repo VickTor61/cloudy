a41e78f97a8208913a45b1c1ee1291897b30e030
Enoma Victor Osasenaga <vickjunior2002@gmail.com>
2024-07-18 01:11:03 +0100

    Build project for submission

---
+++ test/test_helper.rb
@@ -0,0 +1,20 @@
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
+require 'minitest/mock'
+require 'webmock/minitest'
+
+
+module ActiveSupport
+  class TestCase
+    # Run tests in parallel with specified workers
+    parallelize(workers: :number_of_processors)
+
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
+
+    # Add more helper methods to be used by all tests here...
+    include ActiveSupport::Testing::TimeHelpers
+    WebMock.disable_net_connect!(allow_localhost: true)
+  end
+end
